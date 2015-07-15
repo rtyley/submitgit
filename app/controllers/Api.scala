322b402a45d359074633f24eb2cc62956285268a
Roberto Tyley <roberto.tyley@gmail.com>
2015-07-15 23:06:21 +0100

    What gives!?!!?!?!?

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -11,6 +11,7 @@ import play.api.mvc._
 
 import scala.Function.unlift
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -33,18 +34,18 @@ class Api @Inject() (cached: Cached) extends Controller {
   }
 
 
-//  def pullRequestMessages(prId: PullRequestId) = cached(s"$prId messages").default(5) {
-//    Action.async {
-//      val pr = Bot.conn().getRepository(prId.repo.fullName).getPullRequest(prId.num)
-//      val mailingList = Project.byRepoId(prId.repo).mailingList
-//
-//      val messageIds = messageIdsByMostRecentUsageIn(pr)
-//      println(messageIds)
-//      for {
-//        messages <- Future.traverse(messageIds.take(3))(mailingList.lookupMessage)
-//      } yield Ok(toJson(messages.flatten: Seq[MessageSummary]))
-//    }
-//  }
+  def pullRequestMessages(prId: PullRequestId) = cached(s"$prId messages").default(5) {
+    Action.async {
+      val pr = Bot.conn().getRepository(prId.repo.fullName).getPullRequest(prId.num)
+      val mailingList = Project.byRepoId(prId.repo).mailingList
+
+      val messageIds = messageIdsByMostRecentUsageIn(pr)
+      println(messageIds)
+      for {
+        messages <- Future.traverse(messageIds.take(3))(mailingList.lookupMessage)
+      } yield Ok(toJson(messages.flatten: Seq[MessageSummary]))
+    }
+  }
 }
 
 
