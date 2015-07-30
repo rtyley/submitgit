d60ce002bd65425cd326b1bbf1ace33336b1a0bd
Roberto Tyley <roberto.tyley@gmail.com>
2015-07-30 21:30:29 +0100

    Automatically show the ident for the in-reply-to message id on loading

--- public/javascript/messageIdPicker.js
+++ public/javascript/messageIdPicker.js
@@ -1,6 +1,6 @@
 (function ( jQ ) {
 
-    jQ.fn.messageIdPicker = function(options, datasets) {
+    jQ.fn.messageIdPicker = function(options, datasets, onSelect) {
         return this.each(function(index, elem) {
             var inputGroup = jQ(elem).find(".input-group");
             var messageIdent = jQ(elem).find(".message-ident");
@@ -21,12 +21,22 @@
             input.bind('typeahead:render', function () {
                 inputGroup.find("time.timeago").timeago();
             });
-            input.bind('typeahead:select', function (ev, suggestion) {
+            var showIdent = function (suggestion) {
                 messageIdentContent.html(suggestionTemplate(suggestion));
                 messageIdentContent.find("time.timeago").timeago();
                 inputGroup.hide();
                 messageIdent.show();
-            });
+                onSelect(suggestion);
+            };
+            input.bind('typeahead:select', function (ev, suggestion) { showIdent(suggestion) });
+
+            var initialMessageId = input.val();
+            if (initialMessageId !== "") {
+                function showIdentIfSingleResult(datums) {
+                    if (datums.length == 1) { showIdent(datums[0]); }
+                }
+                datasets.source(initialMessageId, showIdentIfSingleResult, showIdentIfSingleResult);
+            }
 
             input.typeahead(options, datasets);
         });
